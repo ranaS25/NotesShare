a7b96a569f42abb62cd83c48cf812780835725b5
My application <ranasankit46@gmail.com>
2024-07-09 14:30:15 +0530

    Created src folder - Moved Component and utils folder to src. - Renamed app.js to App.js and moved it to src. - package.json updated as required.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header';
-import Main from './components/Main';
-import AboutClass from './components/AboutClass';
-import About  from './components/About';
-import Login from './components/Login';
-import Register  from './components/Register';
+import Header from './src/components/Header';
+import Main from './src/components/Main';
+
+import About  from './src/components/About';
+import Login from './src/components/Login';
+import Register  from './src/components/Register';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 
